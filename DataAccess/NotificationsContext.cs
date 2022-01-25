d6905f63fa649cf51014800cc7ada869e9c79e77
Saeed Nemati <sn.1361@gmail.com>
2022-01-25 10:13:46 +0330

    committed by script

--- DataAccess/NotificationsContext.cs
+++ DataAccess/NotificationsContext.cs
@@ -4,9 +4,9 @@ public class NotificationsContext : DatabaseContext
 {
     public override string ConnectionStringName => "Notifications";
 
-    public DbSet<Notification> Notifications { get; set; }
+    public DbSet<Notifications.Notification> Notifications { get; set; }
 
-    public DbSet<NotificationView> NotificationViews { get; set; }
+    public DbSet<Notifications.NotificationView> NotificationViews { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
