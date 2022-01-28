05bb7a91505feeddb4cb20bd581ff478331ec9d4
Saeed Nemati <sn.1361@gmail.com>
2022-01-28 14:48:26 +0330

    committed by script

--- Business/NotificationBusiness.cs
+++ Business/NotificationBusiness.cs
@@ -2,7 +2,7 @@ namespace Notifications;
 
 public class NotificationBusiness: Business<NotificationView, Notification>
 {
-    protected override Repository<Notification> WriteRepository => RepositoryNotification;
+    protected override Repository<Notification> WriteRepository => Repository.Notification;
 
-    protected override ReadRepository<NotificationView> ReadRepository => RepositoryNotificationView;
+    protected override ReadRepository<NotificationView> ReadRepository => Repository.NotificationView;
 }
