1a37815cdf7a81afab3152e3c809458ac03f6db8
Saeed Nemati <sn.1361@gmail.com>
2022-02-12 09:04:48 +0330

    committed by script

--- Business/NotificationBusiness.cs
+++ Business/NotificationBusiness.cs
@@ -2,7 +2,7 @@ namespace Notifications;
 
 public class NotificationBusiness: Business<NotificationView, Notification>
 {
-    protected override Repository<Notification> WriteRepository => Repository.Notification;
+    protected override Write<Notification> Write => Repository.Notification;
 
-    protected override ReadRepository<NotificationView> ReadRepository => Repository.NotificationView;
+    protected override Read<NotificationView> Read => Repository.NotificationView;
 }
--- DataAccess/Repository.cs
+++ DataAccess/Repository.cs
@@ -2,19 +2,19 @@ namespace Notifications;
 
 public class Repository
 {
-    public static Repository<Notifications.Notification> Notification
+    public static Write<Notifications.Notification> Notification
     {
         get
         {
-            return new Repository<Notifications.Notification>(new NotificationsContext());
+            return new Write<Notifications.Notification>(new NotificationsContext());
         }
     }
 
-    public static Repository<Notifications.NotificationView> NotificationView
+    public static Write<Notifications.NotificationView> NotificationView
     {
         get
         {
-            return new Repository<Notifications.NotificationView>(new NotificationsContext());
+            return new Write<Notifications.NotificationView>(new NotificationsContext());
         }
     }
 }
