91272cef9a1b218d7969903570f6112b2642b054
Saeed Nemati <sn.1361@gmail.com>
2022-06-16 16:20:45 +0430

    committed by script

--- Business/NotificationBusiness.cs
+++ Business/NotificationBusiness.cs
@@ -5,4 +5,10 @@ public class NotificationBusiness: Business<NotificationView, Notification>
     protected override Write<Notification> Write => Repository.Notification;
 
     protected override Read<NotificationView> Read => Repository.NotificationView;
+
+    public int GetCount(Guid userGuid)
+    {
+        var count = GetCount(i => i.UserGuid == userGuid);
+        return count;
+    }
 }
